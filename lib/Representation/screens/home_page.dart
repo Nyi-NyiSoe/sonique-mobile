d4299d78615b4a68a68f48d1a95f5e9cb02d55f8
Jack Emrick <nyinyisoe.dev@gmail.com>
2025-04-04 02:32:43 +0700

    feat(auth): added auth , bloc

--- lib/Representation/screens/home_page.dart
+++ lib/Representation/screens/home_page.dart
@@ -5,6 +5,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome to Home Page",
+         
+        ),
+      ),
+    );
   }
 }
