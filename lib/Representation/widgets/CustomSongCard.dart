0a4d1a29d31909a60dae757475a62fa92cbd7de3
Jack Emrick <nyinyisoe.dev@gmail.com>
2025-04-07 17:18:29 +0700

    feat(music-card):added music card

--- lib/Representation/widgets/CustomSongCard.dart
+++ lib/Representation/widgets/CustomSongCard.dart
@@ -5,65 +5,73 @@ class Customsongcard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 100,
-      child: Card(
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                'assets/images/music.png',
-                width: 80,
-                height: 80,
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {},
+      child: SizedBox(
+        height: 100,
+        child: Card(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  'assets/images/music.png',
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Song Title',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const Text(
-                  'Artist Name',
-                  style: TextStyle(fontSize: 15, color: Colors.grey),
-                ),
-              ],
-            ),
-            const Spacer(),
-            //if in playlist display check icon
-            //Icon(Icons.check_circle, color: Colors.green),
-            IconButton(onPressed: () {
-              showModalBottomSheet(
-                useRootNavigator: true,
-                context: context, builder: (context) {
-                return Container(
-                  height: 200,
-                 
-                  child: Column(
-                    children: [
-                      ListTile(
-                        leading: Icon(Icons.play_arrow),
-                        title: Text('Play'),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.add),
-                        title: Text('Add to Playlist'),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.info),
-                        title: Text('Song Info'),
-                      ),
-                    ],
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Song Title',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                );
-              });
-            }, icon: Icon(Icons.more_vert)),
-          ],
+                  const Text(
+                    'Artist Name',
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              //if in playlist display check icon
+              //Icon(Icons.check_circle, color: Colors.green),
+              IconButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                    useRootNavigator: true,
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        height: 200,
+
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading: Icon(Icons.play_arrow),
+                              title: Text('Play'),
+                            ),
+                            ListTile(
+                              leading: Icon(Icons.add),
+                              title: Text('Add to Playlist'),
+                            ),
+                            ListTile(
+                              leading: Icon(Icons.info),
+                              title: Text('Song Info'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                icon: Icon(Icons.more_vert),
+              ),
+            ],
+          ),
         ),
       ),
     );
