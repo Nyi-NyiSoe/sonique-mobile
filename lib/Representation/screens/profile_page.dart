72ae25743611fd7678c702b12de21924bfe1d82c
Jack Emrick <nyinyisoe.dev@gmail.com>
2025-04-26 01:24:59 +0700

    feat(profile-page): added avatar/pick image fucntion

--- lib/Representation/screens/profile_page.dart
+++ lib/Representation/screens/profile_page.dart
@@ -1,17 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:sonique/Data/models/user_model.dart';
 import 'package:sonique/Representation/Bloc/auth_bloc/auth_bloc.dart';
 import 'package:sonique/Representation/Bloc/auth_bloc/auth_event.dart';
+import 'package:sonique/Representation/Bloc/auth_bloc/auth_state.dart';
 import 'package:sonique/Representation/Bloc/user_data_bloc/user_data_bloc.dart';
 import 'package:sonique/Representation/Bloc/user_data_bloc/user_data_state.dart';
+import 'package:sonique/Representation/widgets/CustomButton.dart';
+import 'package:sonique/core/services/routes/routes.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  UserModel? user;
+  XFile? temp_image; // Declare temp_image as a state variable
+
+  @override
   Widget build(BuildContext context) {
-    late final UserModel user;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile Page'),
@@ -27,17 +42,13 @@ class ProfilePage extends StatelessWidget {
         ],
       ),
 
-      body: BlocListener<UserDataBloc, UserDataState>(
+      body: BlocListener<AuthBloc, AuthState>(
         listener: (context, state) {
-          if (state is UserDataErrorState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Error: ${state.error}'),
-                backgroundColor: Colors.red,
-              ),
-            );
-          } else if (state is UserDataFetchedState) {
-            user = state.user;
+          if (state is AuthLoadingState) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(const SnackBar(content: Text('Logged out')));
+            context.go(Routes.login);
           }
         },
         child: BlocBuilder<UserDataBloc, UserDataState>(
@@ -45,13 +56,165 @@ class ProfilePage extends StatelessWidget {
             if (state is UserDataLoadingState) {
               return const Center(child: CircularProgressIndicator());
             } else if (state is UserDataFetchedState) {
-              return Center(child: Text(user.firstName),);
+              user = state.user;
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  updateModal(context);
+                                },
+                                child: CircleAvatar(
+                                  radius: 50,
+                                  backgroundImage: NetworkImage(
+                                    user!.profile_image!,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${user!.firstName} ${user!.lastName}",
+                                    style:
+                                        Theme.of(
+                                          context,
+                                        ).textTheme.displayMedium!,
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    "@${user!.username}",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge!
+                                        .copyWith(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+
+                          const SizedBox(height: 20),
+                          GestureDetector(
+                            onTap: () {
+                              updateModal(context);
+                            },
+                            child: Text(
+                              user!.bio == "" ? "'Add a bio'" : user!.bio!,
+                              style: Theme.of(context).textTheme.labelLarge!
+                                  .copyWith(color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else if (state is UserDataErrorState) {
+              return Center(child: Text(state.error));
             } else {
-              return const Center(child: Text('Error fetching user data'));
+              return const Center(child: Text('Something went wrong!'));
             }
           },
         ),
       ),
     );
   }
+
+  Future<dynamic> updateModal(BuildContext context) {
+    final ImagePicker picker = ImagePicker();
+
+    return showModalBottomSheet(
+      useRootNavigator: true,
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setModalState) {
+            return Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text("Edit profile"),
+                  const SizedBox(height: 20),
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage:
+                            temp_image == null
+                                ? NetworkImage(user!.profile_image!)
+                                    as ImageProvider
+                                : FileImage(File(temp_image!.path))
+                                    as ImageProvider,
+                      ),
+
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: IconButton(
+                          icon: const Icon(Icons.camera_alt),
+                          onPressed: () async {
+                            final hasPermission =
+                                await requestGalleryPermission();
+                            if (!hasPermission) return;
+                            final XFile? image = await picker.pickImage(
+                              source: ImageSource.gallery,
+                            );
+                            if (image != null) {
+                              // Update both parent state and modal state
+                              setState(() {
+                                temp_image = image;
+                              });
+                              setModalState(() {
+                                // This forces the modal to rebuild
+                              });
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  CustomElevatedButton(
+                    child: Text('Save'),
+                    onPressed: () {
+                      setState(() {
+                        temp_image = null;
+                      });
+
+                      setModalState(() {
+                        // This forces the modal to rebuild
+                      });
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+Future<bool> requestGalleryPermission() async {
+  var status = await Permission.photos.request(); // iOS
+  if (status.isGranted) return true;
+
+  status = await Permission.storage.request(); // Android
+  return status.isGranted;
 }
