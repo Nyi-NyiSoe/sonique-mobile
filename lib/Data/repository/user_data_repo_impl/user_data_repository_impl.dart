d3e992da4f991914ef7629a1e1e440c787954bc1
Jack Emrick <nyinyisoe.dev@gmail.com>
2025-05-20 02:17:04 +0700

    feat(profile-page):added upload image feature

--- lib/Data/repository/user_data_repo_impl/user_data_repository_impl.dart
+++ lib/Data/repository/user_data_repo_impl/user_data_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:image_picker/image_picker.dart';
 import 'package:sonique/Data/models/user_model.dart';
 import 'package:sonique/Data/source/user_data_repo/user_remote_data.dart';
 import 'package:sonique/Domain/repository/user_data_repository.dart';
@@ -14,5 +15,30 @@ class UserDataRepositoryImpl implements UserDataRepository {
     }
   }
 
-  
+  @override 
+  Future<String> updateUserImage(XFile? profile_image) async {
+    try {
+      return await userRemoteData.updateUserImage(profile_image);
+    } catch (e) {
+      throw Exception('Failed to update user image: $e');
+    }
+  }
+  @override
+  Future<void> updateUserData(
+    String? bio,
+    String? firstName,
+    String? lastName,
+    String? username,
+  ) async {
+    try {
+      await userRemoteData.updateUserDetails(
+        bio,
+        firstName,
+        lastName,
+        username,
+      );
+    } catch (e) {
+      throw Exception('Failed to update user data: $e');
+    }
+  }
 }
