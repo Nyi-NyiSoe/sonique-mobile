713966fc8f18b1c6c80a546c0285906c21989e9a
unknown <nyinyisoe.dev@gmail.com>
2025-08-20 18:29:21 +0700

    feat/song-data: added upload song function

--- lib/Data/source/song_data_repo/song_remote_data.dart
+++ lib/Data/source/song_data_repo/song_remote_data.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'package:sonique/Data/models/genre_model.dart';
 import 'package:sonique/Data/models/song_response_model.dart';
 import 'package:sonique/Data/source/auth_repo/auth_local_data_source.dart';
@@ -13,6 +14,7 @@ class SongRemoteData {
   late String getSongByIdUrl;
   late String getGenreUrl;
   late String uploadGenreUrl;
+  late String uploadSongUrl;
   final AuthLocalDataSource authLocalDataSource;
 
   SongRemoteData({required this.client, required this.authLocalDataSource}) {
@@ -21,12 +23,18 @@ class SongRemoteData {
 
     getGenreUrl = dotenv.env['GET_SONG_GENRES_URL'] ?? '';
     uploadGenreUrl = dotenv.env['UPLOAD_SONG_GENRE_URL'] ?? '';
+
+    uploadSongUrl = dotenv.env['UPLOAD_SONG_URL'] ?? '';
     if (getSongUrl.isEmpty) {
       throw Exception('API URL is not set in .env file');
     }
 
     if (getGenreUrl.isEmpty) {
       throw Exception('genre URL is not set in .env file');
+    }
+
+    if (uploadSongUrl.isEmpty) {
+      throw Exception('song URL is not set in .env file');
     }
   }
   Future<SongResponseModel> getAllSongs() async {
@@ -156,4 +164,58 @@ class SongRemoteData {
     }
   }
 
+  Future<void> uploadSong(
+    XFile audioFile,
+    XFile coverImage,
+    String genreId,
+    String title,
+  ) async {
+    final currentUser = await authLocalDataSource.getUser();
+    final refreshToken = currentUser.refreshToken;
+    final token = currentUser.token;
+
+    if (refreshToken == null || token == null) {
+      throw Exception('Refresh token or access token is missing');
+    }
+
+    final headers = {
+      'Content-Type': 'application/json',
+      'Cookie': 'token=$token;refreshToken=$refreshToken',
+    };
+
+    try {
+      final request = http.MultipartRequest(
+        'POST',
+        Uri.parse('$uploadSongUrl'),
+      );
+      request.headers.addAll(headers);
+      if (currentUser.isArtist) {
+        request.fields['artistId'] = currentUser.userId.toString();
+      }
+      request.fields['genreId'] = genreId;
+      request.fields['title'] = title;
+
+      //add audio file
+      request.files.add(
+        await http.MultipartFile.fromPath('audio', audioFile.path),
+      );
+
+      //add cover image
+      request.files.add(
+        await http.MultipartFile.fromPath('coverImage', coverImage.path),
+      );
+
+      //send req
+      final response = await request.send();
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        log('Success');
+        log(response.toString());
+      } else {
+        log(response.statusCode.toString());
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 }
