c8c5ac7b37628a4bae9012c6f15acd29bcf1f5e9
unknown <nyinyisoe.dev@gmail.com>
2025-09-21 03:15:45 +0700

    feat/album: added album page, album bloc,integrated with get album end point

--- lib/Representation/widgets/CustomAlbumCard.dart
+++ lib/Representation/widgets/CustomAlbumCard.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sonique/Representation/Bloc/album_bloc/album_detail_bloc/album_detail_bloc.dart';
+import 'package:sonique/Representation/Bloc/album_bloc/album_detail_bloc/album_detail_event.dart';
 
 class CustomAlbumCard extends StatelessWidget {
-  const CustomAlbumCard({super.key, required this.imageUrl});
+  const CustomAlbumCard({super.key, required this.imageUrl,required this.albumId});
   final String imageUrl;
+  final int albumId;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 12),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(12),
-        child: Image(image: AssetImage(imageUrl)),
+    return GestureDetector(
+      onTap: () {
+        context.read<AlbumDetailBloc>().add(FetchAlbumDetailEvent(albumId));
+         context.go('/home/albumDetail'); 
+      },
+      child: Container(
+        width: MediaQuery.sizeOf(context).width * 0.5,
+        margin: const EdgeInsets.only(right: 12),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        child: ClipRRect(
+          
+          borderRadius: BorderRadius.circular(12),
+          child: Image.network(imageUrl),
+        ),
       ),
     );
   }
